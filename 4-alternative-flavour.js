8d82b2acc0fbc6e42942a7859333f5910b0bbe6e
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2025-02-24 11:46:13 +0530

    Added code for alternative flavour and updated app.js to be compatible.

--- 4-alternative-flavour.js
+++ 4-alternative-flavour.js
@@ -1,4 +1,4 @@
-module.exports.items = [item1, item2];
+module.exports.items = ["item1", "item2"];
 
 const person = {
     name: "bob"
--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 const names = require(`./2-names`) 
 const sayHi = require(`./3-utils`)
+const data = require(`./4-alternative-flavour`)
+
+console.log(data)
 
 sayHi(names.john)
 sayHi(names.peter)
