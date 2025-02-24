6e5f2e6310dd25636cdc33a071c4944f50697b56
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2025-02-24 11:50:38 +0530

    Added 'mind-grenade'

---
+++ 5-mind-grenade.js
@@ -0,0 +1,8 @@
+const numero_uno = 55;
+const numero_dos = 20;
+
+const SubtractValues = () => {
+    console.log(`The difference between the two numbers is ${numero_uno - numero_dos}`);
+}
+
+SubtractValues()
