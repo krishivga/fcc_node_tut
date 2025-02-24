8634a5019be44215f1476662868e3e0642f51110
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2025-02-24 12:58:43 +0530

    Saved everything from app.js to pathmodule js

--- app.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-console.log(path.sep);
-
-const filePath = path.join("/pathExample/", "innerFolder", "test.txt")
-console.log(filePath);
- 
-const baseName = path.basename(filePath)
-console.log(baseName);
