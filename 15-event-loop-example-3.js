0584308dadd5179ef713888f1d97bd7867ecf605
Krishiv Agarwal <krishiv.agarwal@cgpworldwide.com>
2025-02-28 13:42:38 +0530

    wrote code for event loop example 3

---
+++ 15-event-loop-example-3.js
@@ -0,0 +1,7 @@
+// Example with loops
+
+setInterval(() => {
+    console.log('hello world')
+}, 2000)
+console.log("I will run first")
+// Will run until killed by Ctrl C
--- app.js
+++
@@ -1,8 +0,0 @@
-// Example for event loops using setTimeout()
-
-console.log("first")
-setTimeout(() =>  {
-    console.log("second") // Callback happens at the end
-}, 0)
-console.log("third")
-// completed and exiting system process
